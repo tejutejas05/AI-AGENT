14fe79a285e7e550a4d6419b5809398fab44a61b
tejutejas05 <tejasmr05@gmail.com>
2026-03-26 22:53:45 +0530

    agent/core.py updated

--- agent/core.py
+++ agent/core.py
@@ -1,8 +1,13 @@
 # Brain of the agent (LLM)
 
 from tools.calculator import calculate
+# this will import the tools from the tools folder
 from tools.time_tool import get_time
 from llm.openai_client import ask_llm
+
+
+
+# the Prompt template we are going to use for this agent
 
 def run_agent(user_input):
 
@@ -15,6 +20,6 @@ def run_agent(user_input):
 
     else:
         return ask_llm(user_input)
+        #here the llm folder should be get accessed 
 
 
-        
